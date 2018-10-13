a319e6ab2fb7333857cfe66644d6ecd9f3318527
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-13 18:34:58 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -14,8 +14,10 @@ def create_project_hash
   projects = {}
 
   kickstarter.css("li.project.grid_4").each do |project|
-  projects[project] = {}
-end #end do loop
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title.to_sym] = {}
+  end # end do loop
 
 projects
+
 end
