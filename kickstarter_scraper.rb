a6a2d995d81e2ee63581f1cffb012935b57dddd8
BRIAN BETANCOURT <bbetancova@gmail.com>
2018-10-13 18:19:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -2,6 +2,7 @@ require 'nokogiri'
 
 def create_project_hash
   html = File.read('fixtures/kickstarter.html')
+  #get the webpage and store it in a veriable
   kickstarter = Nokogiri::HTML(html)
 
 end
